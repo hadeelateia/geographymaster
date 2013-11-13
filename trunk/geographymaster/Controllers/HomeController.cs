a5602bae2e55e62d3c0ad5c0cd51184afa49e730
biserka.cvetkovska@gmail.com <biserka.cvetkovska@gmail.com@ebef5605-4c3b-f058-e497-b6eab9512e10>
2013-11-13 09:03:55 +0000

    initial commit

---
+++ trunk/geographymaster/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace geographymaster.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+    }
+}
