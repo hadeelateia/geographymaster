a2fec13ac046b95b6b6678cf0097c4868111d220
biserka.cvetkovska@gmail.com <biserka.cvetkovska@gmail.com@ebef5605-4c3b-f058-e497-b6eab9512e10>
2013-12-09 21:24:13 +0000

    added new QuestionViewModel + entering Category

---
+++ trunk/geographymaster/Models/QuestionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace geographymaster.Models
+{
+    public class QuestionViewModel
+    {
+        public Question QuestionDetails { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        
+        public long CategoryId { get; set; }
+        public long QuestionId { get; set; }
+    }
+}
